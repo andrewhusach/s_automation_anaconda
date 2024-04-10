6df30858312b3933d9f8a8ce7bb29645d5778a00
andrewhusach <137443190+andrewhusach@users.noreply.github.com>
2024-04-10 11:02:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,26 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Flask! Testing autodeployment'
+    return 'Hello from Flask!'
 
 @app.route('/github-deploy', methods=['POST'])
 def github_deploy():
-    return 'POST request to /github-deploy received', 200
+    try:
+        # Change to your project's directory
+        os.chdir('/home/agusach/mydir')
+
+        # Pull the latest changes from your GitHub repository
+        subprocess.check_call(['git', 'pull'])
+
+        # Touch the WSGI file to reload the web application
+        subprocess.check_call(['touch', '/var/www/agusach_pythonanywhere_com_wsgi.py'])
+
+        return 'Deployment script executed successfully.', 200
+    except subprocess.CalledProcessError as e:
+        # If an error occurs during the subprocess calls, log it or print it.
+        # Consider logging the error to a file for production use
+        print(f"Error occurred: {e}")
+        return 'Failed to execute deployment script.', 500
 
 if __name__ == '__main__':
     app.run(debug=True, port=5002)
