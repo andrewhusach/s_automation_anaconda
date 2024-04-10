7ca49636e5914c92fda6fd73f22e542d8a80a74f
andrewhusach <137443190+andrewhusach@users.noreply.github.com>
2024-04-10 10:49:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Flask!'
+    return 'Hello from Flaskaosnjfd!'
 
 @app.route('/github-deploy', methods=['POST'])
 def github_deploy():
